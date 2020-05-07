627b939447e745fb3effd2145f65a301218f3c2c
iv <consmebius@gmail.com>
2020-05-07 22:51:29 +0300

    .

--- app.js
+++ app.js
@@ -31,5 +31,43 @@ function El() {
     console.log(div);
 }
 
-El()
+El();
 
+/**
+ * method - "GET" or "POST" 
+ * url - "https://jsonplaceholder.typicode.com/todos/"
+ * cb - (error, response) => {}
+ * object - {}
+ */
+function GetAjax(method, url, cb, object) {
+    try {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.addEventListener("load", () => {
+            if (Math.floor(xhr.status / 100) !== 2) {
+                cb(`Error: ${xhr.status}`, xhr);
+                return;
+            }
+            const response = JSON.parse(xhr.responseText);
+            cb(null, response);
+        });
+
+        if (object) {
+            xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+            xhr.send(JSON.stringify(object));
+        } else {
+            xhr.send();
+        }
+    } catch (error) {
+        cb(error);
+    }
+
+}
+
+GetAjax("GET", "https://jsonplaceholder.typicode.com/users", (err, response) => {
+    if (err) {
+        console.log(err, response);
+        return;
+    }
+    console.log(response);
+});
